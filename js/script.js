2e8d365cd9b06d271b46edea040bdeb43d2986af
Zahirr <zohirlatipov2004@gmail.com>
2024-02-01 04:18:53 +0500

    довил к формам обратный ответ

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 function showForm() {
     var overlay = document.getElementById('overlay');
     var formContainer = document.getElementById('formContainer');
+    var nameInput = document.getElementById("name");
+
+    nameInput.value = '';
 
     overlay.style.display = 'block';
     formContainer.style.display = 'block';
@@ -13,30 +16,116 @@ function submitForm() {
     var formContainer = document.getElementById('formContainer');
     var name = document.getElementById("name").value;
     var userNameElement = document.getElementById("userName");
-    
+    var nameInput = document.getElementById("name");
+
     if (name.trim() === "") {
         alert("Пожалуйста, введите ваше имя.");
         return;
     }
-    
 
     callForm.style.display = 'none';
     thankYouMessage.style.opacity = '1';
     thankYouMessage.style.display = 'block';
     userNameElement.innerHTML = name;
 
-    setTimeout(function() {
+    setTimeout(function () {
         thankYouMessage.style.display = 'none';
         hideForm();
     }, 10000);
 }
-
-function returnToSite() {
+function hideForm() {
     var overlay = document.getElementById('overlay');
     var formContainer = document.getElementById('formContainer');
 
     overlay.style.display = 'none';
     formContainer.style.display = 'none';
-
-    alert("Возвращаемся на сайт...");
 }
+
+
+function returnToSite() {
+    var overlay = document.getElementById('overlay');
+    var formContainer = document.getElementById('formContainer');
+    var callForm = document.getElementById('callForm');
+    overlay.style.display = 'none';
+    formContainer.style.display = 'none';
+    var userNameElement = document.getElementById("userName");
+    userNameElement.innerHTML = '';
+    callForm.style.display = 'block';
+}
+
+
+
+
+
+
+
+
+function submitCustomApplication() {
+    var customFormName = document.getElementById("customName").value;
+    var customFormPhone = document.getElementById("customPhone").value;
+    var customFormEmail = document.getElementById("customEmail").value;
+    var customFormDeliveryMethod = document.querySelector('input[name="customDeliveryMethod"]:checked');
+
+    if (!customFormName || !customFormPhone || !customFormEmail || !customFormDeliveryMethod) {
+        alert('Пожалуйста, заполните все обязательные поля.');
+        return;
+    }
+    var customThankYouMessage = document.getElementById('customThankYouMessage');
+    customThankYouMessage.innerHTML = `<p class="thankYouText">Спасибо, ${customFormName} Мы получили вашы данные! Мы свяжемся с вами и произведём оценку</p>`;
+
+    customThankYouMessage.style.display = 'block';
+
+    setTimeout(function () {
+        customThankYouMessage.style.display = 'none';
+        resetCustomForm();
+    }, 5000);
+}
+
+function resetCustomForm() {
+    var customForm = document.getElementById('customApplicationForm');
+    customForm.reset();
+}
+
+
+
+function toggleDescription(descriptionBlockId) {
+    var descriptionBlock = document.getElementById(descriptionBlockId);
+
+    // Инвертируем состояние блока с описанием при каждом клике
+    if (descriptionBlock.style.display === 'block') {
+        descriptionBlock.style.display = 'none'; // если открыто, то закрываем
+    } else {
+        descriptionBlock.style.display = 'block'; // если закрыто, то открываем
+    }
+}
+
+
+
+
+
+
+function submitApplication() {
+    var fullName = document.getElementById('fullName').value;
+    var phoneNumber = document.getElementById('phoneNumber').value;
+    var emailAddress = document.getElementById('emailAddress').value;
+
+    if (!fullName || !phoneNumber || !emailAddress) {
+        alert('Пожалуйста, заполните все обязательные поля.');
+        return;
+    }
+
+
+    document.getElementById('submittedFullName').textContent = fullName;
+    document.getElementById('submittedPhoneNumber').textContent = phoneNumber;
+    document.getElementById('submittedEmailAddress').textContent = emailAddress;
+
+    document.getElementById('applicationForm').style.display = 'none';
+    var thankYouMessagee = document.getElementById('thankYouMessagee');
+    thankYouMessagee.style.display = 'block';
+
+    setTimeout(function () {
+        document.getElementById('applicationForm').style.display = 'flex';
+        thankYouMessagee.style.display = 'none';
+    }, 5000);
+}
+
